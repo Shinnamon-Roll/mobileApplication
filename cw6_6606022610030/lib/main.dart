971cf7ec286acae1bc60f71a9ecc14526c63ce73
-Shinnamon- <chimonkung0@gmail.com>
2026-01-13 11:16:06 +0700

    Add: workshop

--- cw6_6606022610030/lib/main.dart
+++ cw6_6606022610030/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
-import 'Shopping.dart';
 
 class MyForm extends StatefulWidget {
-  const MyForm({key? key}) : super(key: key);
+  const MyForm({Key? key}) : super(key: key);
   @override
   State<MyForm> createState() => _MyFormState();
 }
@@ -18,9 +17,142 @@ class _MyFormState extends State<MyForm> {
   bool? _checkBox, _listTileCheckBox = false;
 
   @override
+  void initState() {
+    super.initState();
+    _checkBox = false;
+    _listTileCheckBox = false;
+  }
+
+  @override
   void dispose() {
     _productNameController.dispose();
     _productPriceController.dispose();
     super.dispose();
+  }
+
+  void _updateTextField() {
+    setState(() {
+      _productName = _productNameController.text;
+      _productPrice = _productPriceController.text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("PRODUCT")), // AppBar
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            const Text("PRODUCT App", style: TextStyle(fontSize: 30)), // Text
+            const Text("Add Product detail in the form "),
+            SizedBox(height: 10.0),
+            MyTextField(
+              fieldName: "Product Name",
+              myController: _productNameController,
+              myIcon: Icons.pin_drop_sharp,
+              prefixIconColor: Colors.deepPurple.shade300,
+            ), // MyTextField
+            SizedBox(height: 20.0),
+            MyTextField(
+              fieldName: "Product Description",
+              myController: _productPriceController,
+              myIcon: Icons.pin_drop_sharp,
+              prefixIconColor: Colors.deepPurple.shade300,
+            ), // MyTextField
+            SizedBox(height: 20.0),
+            // Two Types of Check Box
+
+            //1. check box
+            Checkbox(
+              checkColor: Colors.white,
+              activeColor: Colors.deepPurple,
+              tristate: true,
+              value: _checkBox,
+              onChanged: (val) {
+                setState(() {
+                  _checkBox = val;
+                });
+              },
+            ), // Checkbox
+            //2. check box list tile
+            CheckboxListTile(
+              value: _listTileCheckBox,
+              title: Text("Top Product"),
+              onChanged: (val) {
+                setState(() {
+                  _listTileCheckBox = val;
+                });
+              },
+              controlAffinity: ListTileControlAffinity.leading,
+            ), // CheckboxListTile
+
+            SizedBox(height: 20.0),
+            myBtn(context),
+            SizedBox(height: 40.0),
+          ],
+        ), // ListView
+      ), // Container
+    ); // Scaffold
+  }
+
+  Center myBtn(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          _updateTextField();
+        },
+        child: const Text("Add Product"),
+      ),
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({
+    Key? key,
+    required this.fieldName,
+    required this.myController,
+    this.myIcon = Icons.verified_user_outlined,
+    this.prefixIconColor = Colors.blueAccent,
+  }) : super(key: key);
+
+  final TextEditingController myController;
+  final String fieldName;
+  final IconData myIcon;
+  final Color prefixIconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: myController,
+      decoration: InputDecoration(
+        labelText: fieldName,
+        prefixIcon: Icon(myIcon, color: prefixIconColor), // Icon
+        border: const OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.deepPurple.shade300),
+        ), // OutlineInputBorder
+        labelStyle: const TextStyle(color: Colors.deepPurple),
+      ), // InputDecoration
+    ); // TextFormField
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product App',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const MyForm(),
+    );
   }
 }
