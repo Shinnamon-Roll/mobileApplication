e8670896a15d762d19acbc3845ffb9c08413697f
-Shinnamon- <chimonkung0@gmail.com>
2026-02-03 21:00:28 +0700

    feat: Add new images, update Podfile, and implement user model with database support

---
+++ cw7_6606022610030/lib/Oldmain.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterDemo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'List Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class Data {
+  int id;
+  String name;
+  DateTime t;
+  Data(this.id, this.name, this.t);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int img = 1;
+  String txt = '';
+  List<Data> mylist = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: img,
+                  onChanged: (int? value) {
+                    setState(() {
+                      img = 1;
+                    });
+                  },
+                ),
+                const CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage('assets/images/ig.png'),
+                ),
+              ],
+            ),
+            const TextField(),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  txt = "Add item Success";
+                  mylist.add(Data(img, '1', DateTime.now()));
+                });
+              },
+              child: const Text('Add item'),
+            ),
+            Text(txt, textScaleFactor: 2),
+            SizedBox(
+              width: double.infinity,
+              height: 550,
+              child: ListView.builder(
+                itemCount: mylist.length,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: double.infinity,
+                    height: 80,
+                    child: Card(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      color: Colors.primaries[index % Colors.primaries.length],
+                      child: ListTile(
+                        leading: const CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage(
+                            'assets/images/rocket.png',
+                          ),
+                        ),
+                        title: Text('Title Text (${mylist[index].id})'),
+                        subtitle: Text(mylist[index].t.toString()),
+                        trailing: const Icon(Icons.delete_rounded),
+                        onTap: () {
+                          setState(() {
+                            txt = "Title Text ($index) is remove";
+                            mylist.removeAt(index);
+                          });
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- cw7_6606022610030/test/widget_test.dart
+++ cw7_6606022610030/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cw7_6606022610030/main.dart';
+import 'package:cw7_6606022610030/Oldmain.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
